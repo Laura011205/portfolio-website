959ddc2355828a704048b52442938b3d7e32e4d6
Laura Lu <lulaura011205@gmail.com>
2022-02-28 20:04:38 -0800

    updated background color

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
