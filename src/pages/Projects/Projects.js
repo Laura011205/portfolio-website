37867a50f7a79e3122da543e7e869cc45ab90462
Laura Lu <lulaura011205@gmail.com>
2022-02-28 20:33:27 -0800

    updated projects page

---
+++ src/pages/Projects/Projects.js
@@ -0,0 +1,12 @@
+import Navbar from "../../components/Navbar/Navbar";
+import "./Projects.css";
+function Projects() {
+  return (
+    <>
+      <Navbar />
+      <p className="update-alert">Work in progress, check back later!</p>
+    </>
+  );
+}
+
+export default Projects;
