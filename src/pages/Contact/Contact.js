f63278bd8323f0a48079463204741c053fcb8178
Laura Lu <lulaura011205@gmail.com>
2022-02-28 20:33:05 -0800

    updated contact page

---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,12 @@
+import Navbar from "../../components/Navbar/Navbar";
+import "./Contact.css";
+function Contact() {
+  return (
+    <>
+      <Navbar />
+      <p className="update-alert">Work in progress, check back later!</p>
+    </>
+  );
+}
+
+export default Contact;
