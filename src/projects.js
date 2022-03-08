4570ffdd141292785e379a376ced6ed1ce5687c9
Laura Lu <lulaura011205@gmail.com>
2022-03-07 23:58:01 -0800

    added project information

---
+++ src/projects.js
@@ -0,0 +1,43 @@
+import discordBotImg from "./assets/discord-bot-img.png";
+
+const projects = [
+  {
+    name: "Annoying Discord Bot",
+    image: discordBotImg,
+    description:
+      "A Discord bot that follows a specified user in a server and serenades the user when they join a voice channel. Built using JavaScript and the discord.js module (v13)",
+    repo: "https://github.com/Laura011205/annoying-discord-bot",
+  },
+  {
+    name: "",
+    image: "",
+    description: "",
+    repo: "",
+  },
+  {
+    name: "",
+    image: "",
+    description: "",
+    repo: "",
+  },
+  {
+    name: "",
+    image: "",
+    description: "",
+    repo: "",
+  },
+  {
+    name: "",
+    image: "",
+    description: "",
+    repo: "",
+  },
+  {
+    name: "",
+    image: "",
+    description: "",
+    repo: "",
+  },
+];
+
+export default projects;
