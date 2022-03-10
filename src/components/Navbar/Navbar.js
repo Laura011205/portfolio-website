969f03af5f2a440561291780c40e2159bc868d60
Laura Lu <lulaura011205@gmail.com>
2022-03-09 17:56:58 -0800

    removed unused import

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import "./Navbar.css";
 import MobileNavbar from "../MobileNavbar/MobileNavbar";
 import DesktopNavbar from "../DesktopNavbar/DesktopNavbar";
 
