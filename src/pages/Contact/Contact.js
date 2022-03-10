19035da153be96b90388b7287048ec8a17442163
Laura Lu <lulaura011205@gmail.com>
2022-03-09 17:50:31 -0800

    updated contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,11 +1,22 @@
 import Navbar from "../../components/Navbar/Navbar";
 import "./Contact.css";
+import ContactForm from "../../components/ContactForm/ContactForm";
+
 function Contact() {
   return (
-    <>
+    <div className="contact-page">
       <Navbar />
-      <p className="update-alert">Work in progress, check back later!</p>
-    </>
+      <div className="contact-content">
+        <div className="contact-content-wrapper">
+          <h4 className="contact-title">Get in Touch</h4>
+          <p className="contact-text">
+            I am open to Fall 2022 internship opportunities and freelance
+            projects. <br /> Feel free to reach out :)
+          </p>
+          <ContactForm />
+        </div>
+      </div>
+    </div>
   );
 }
 
